fe5777f98e76061e95bc3f3466c83f93f3cf84f0
Harakirigi <nkomyt2@gmail.com>
2025-04-04 01:40:40 +0500

    v0.5

--- draft/draft.py
+++ draft/draft.py
@@ -1,93 +1,117 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import pprint
+
+CYAN = '\033[36m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+RESET = '\033[0m'
 
 
 url = "https://stat.gov.kz"
-parse_url = "https://stat.gov.kz/en/industries/economy/prices/dynamic-tables/"
+parse_url = "https://stat.gov.kz/en/industries/labor-and-income/stat-empt-unempl/dynamic-tables/"
 
-def get_request():
+def get_request(parse_url):
     try:
         response = requests.get(parse_url, headers={'User-Agent': 'Mozilla/5.0'})
         response.raise_for_status()
         
         soup = BeautifulSoup(response.text, "lxml")
-        return get_body(soup, parse_url)
+        print(CYAN + 'request obtained successfully' + RESET)
+        return soup
     except requests.exceptions.RequestException as error:
-        print(f"Request failed: {error}")
+        print(RED + f"Request failed: {error}" + RESET)
     except Exception as error:
-        print(f"Unexpected error: {error}")
+        print(RED + f"Unexpected error: {error}" + RESET)
+
 
 def get_body(soup, parse_url):
     try:
-        table_body = soup.find_all(class_='divTableBody')
+        precheck_table_body = soup.find_all(class_='divTableBody')
 
-        if len(table_body) > 1:
-            for body in table_body:
-                rows = body.find_all(class_='divTableRow')
-                if not rows:
-                    print(f"Could not find rows in {parse_url}")
+        if len(precheck_table_body) > 1:
+            table_body = soup.find_all(class_='divTableBody')
+
+            bodies_list = []
+            for table in table_body:
+                bodies = table.find_all(class_='divTableRow')
+                if not bodies:
+                    print(f"Could not find bodies in {parse_url}")
                     return
-                
-                get_link(rows)
+                bodies_list.extend(bodies)
 
-            return
+            print(CYAN + 'body list obtained successfully' + RESET)
+            return bodies_list
 
-        rows = table_body.find_all(class_='divTableRow')
-        if not rows:
-            print(f"Could not find rows in {parse_url}")
+        table_body = soup.find(class_='divTableBody')
+
+        bodies = table_body.find_all(class_='divTableRow')
+        if not bodies:
+            print(f"Could not find bodies in {parse_url}")
             return
         
-        return get_link(rows)
+        print(CYAN + 'body obtained successfully' + RESET)
+        return bodies
 
     except Exception as error:
-        print('Error while getting body: ', error)
+        print(RED + 'Error while getting body: ', error, RESET)
 
-def get_link(rows):
+
+def get_link(bodies):
     try:        
         links = {}
-        for row in rows:
-            link = row.find('a')
+        for body in bodies:
+            link = body.find('a')
             api_link = link['href']
-            link_title = link.text.strip().replace('"','').replace(' ','_')
+            link_title = link.text.strip().replace('"','').replace(' ','_').replace('\\t','').replace('\\n','')
 
             if not link_title or link_title == 'xls':
-                link_title = row.find(class_='divTableCell').text.strip().replace('"','').replace(' ','_')
+                link_title = body.find(class_='divTableCell').text.strip().replace('"','').replace(' ','_').replace('\\n','')
                 link_title = 'Dynamic table ' + link_title
                 
             links[link_title] = url + api_link
 
+        print(CYAN + 'links obtained successfully' + RESET)
         return links
             
     except Exception as error:
-        print('Error while getting link: ', error)
+        print(RED + 'Error while getting link: ', error, RESET)
 
 
-
-links = get_request()
-print(links)
-
-# def download_excel_file(title, link, save_path='downloads'):
-#     os.makedirs(save_path, exist_ok=True)
-#     try:
-#         response = requests.get(link, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
-#         response.raise_for_status()
+def download_excel_file(title, link, save_path='downloads'):
+    os.makedirs(save_path, exist_ok=True)
+    try:
+        response = requests.get(link, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
+        response.raise_for_status()
         
-#         filename = title + '.xlsx'
-#         filepath = os.path.join(save_path, filename)
+        filename = title + '.xlsx'
+        filepath = os.path.join(save_path, filename)
         
-#         with open(filepath, 'wb') as f:
-#             for chunk in response.iter_content(chunk_size=8192):
-#                 f.write(chunk)
+        with open(filepath, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
         
-#         print(f"Successfully downloaded: {filename}")
-#         return filepath
-#     except Exception as e:
-#             print(f"Failed to download {link}: {str(e)}")
-#             return None
+        print(CYAN + f"Successfully downloaded: {filename}" + RESET)
+        return filepath
+    except Exception as e:
+            print(RED + f"Failed to download {link}: {str(e)}" + RESET)
+            return None
     
-# for title, link in links.items():
-#     download_excel_file(title, link)
+
+
+
+soup = get_request(parse_url)
+
+bodies = get_body(soup, parse_url)
+
+links = get_link(bodies)
+
+for title, link in links.items():
+    download_excel_file(title, link)
+
+
+
 
 
