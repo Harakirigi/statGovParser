77bcefa15cec07c54a5863a69d58fdeb325d7842
Harakirigi <nkomyt2@gmail.com>
2025-04-04 01:01:03 +0500

    v0.3

--- draft/draft.py
+++ draft/draft.py
@@ -5,7 +5,7 @@ import pprint
 
 
 url = "https://stat.gov.kz"
-parse_url = "https://stat.gov.kz/en/industries/economy/prices/spreadsheets/"
+parse_url = "https://stat.gov.kz/en/industries/economy/prices/dynamic-tables/"
 
 def get_request():
     try:
@@ -13,21 +13,39 @@ def get_request():
         response.raise_for_status()
         
         soup = BeautifulSoup(response.text, "lxml")
-        return get_link(soup, parse_url)
+        return get_body(soup, parse_url)
     except requests.exceptions.RequestException as error:
         print(f"Request failed: {error}")
     except Exception as error:
         print(f"Unexpected error: {error}")
 
-def get_link(soup, parse_url):
+def get_body(soup, parse_url):
     try:
-        table_body = soup.find(class_='divTableBody')
+        table_body = soup.find_all(class_='divTableBody')
+
+        if len(table_body) > 1:
+            for body in table_body:
+                rows = body.find_all(class_='divTableRow')
+                if not rows:
+                    print(f"Could not find rows in {parse_url}")
+                    return
+                
+                get_link(rows)
+
+            return
 
         rows = table_body.find_all(class_='divTableRow')
         if not rows:
             print(f"Could not find rows in {parse_url}")
             return
         
+        return get_link(rows)
+
+    except Exception as error:
+        print('Error while getting body: ', error)
+
+def get_link(rows):
+    try:        
         links = {}
         for row in rows:
             link = row.find('a')
@@ -48,27 +66,28 @@ def get_link(soup, parse_url):
 
 
 links = get_request()
+print(links)
 
-def download_excel_file(title, link, save_path='downloads'):
-    os.makedirs(save_path, exist_ok=True)
-    try:
-        response = requests.get(link, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
-        response.raise_for_status()
+# def download_excel_file(title, link, save_path='downloads'):
+#     os.makedirs(save_path, exist_ok=True)
+#     try:
+#         response = requests.get(link, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
+#         response.raise_for_status()
         
-        filename = title + '.xlsx'
-        filepath = os.path.join(save_path, filename)
+#         filename = title + '.xlsx'
+#         filepath = os.path.join(save_path, filename)
         
-        with open(filepath, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
+#         with open(filepath, 'wb') as f:
+#             for chunk in response.iter_content(chunk_size=8192):
+#                 f.write(chunk)
         
-        print(f"Successfully downloaded: {filename}")
-        return filepath
-    except Exception as e:
-            print(f"Failed to download {link}: {str(e)}")
-            return None
+#         print(f"Successfully downloaded: {filename}")
+#         return filepath
+#     except Exception as e:
+#             print(f"Failed to download {link}: {str(e)}")
+#             return None
     
-for title, link in links.items():
-    download_excel_file(title, link)
+# for title, link in links.items():
+#     download_excel_file(title, link)
 
 
