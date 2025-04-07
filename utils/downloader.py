a9e5c0e3c0ac674a1ed98e0a75eae6174ab32936
Harakirigi <nkomyt2@gmail.com>
2025-04-07 12:03:29 +0500

    v0.9

--- utils/downloader.py
+++ utils/downloader.py
@@ -52,4 +52,29 @@ def check_for_links(links_to_stats):
 
 all_links = ['https://stat.gov.kz/en/industries/environment/stat-eco/', "https://stat.gov.kz/en/industries/economy/national-accounts/",  'https://stat.gov.kz/en/climate-change/']
 
-check_for_links(all_links)
+
+
+def get_body(soup, parse_url):
+    try:
+        table_body = soup.find_all(class_='divTableBody')
+
+        bodies_list = []
+        for table in table_body:
+            bodies = table.find_all(class_='divTableRow')
+            if not bodies:
+                print(f"Could not find bodies in {parse_url}")
+                return
+            bodies_list.extend(bodies)
+
+        print(CYAN + 'body list obtained successfully' + RESET)
+        return bodies_list
+
+    except Exception as error:
+        print(RED + 'Error while getting body: ', error, RESET)
+
+
+
+link_page_to_parse = check_for_links(all_links)
+
+for link, page in link_page_to_parse.items():
+    get_body(page, link)
