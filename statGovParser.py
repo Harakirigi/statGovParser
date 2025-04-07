9e960cd0390c841f055e4a9ae57ad8586968b0d4
Harakirigi <nkomyt2@gmail.com>
2025-04-07 23:21:26 +0500

    complete version - v1 | executable app created

--- statGovParser.py
+++ statGovParser.py
@@ -4,7 +4,7 @@ from gui import root
 base_url = "https://stat.gov.kz"
 url = "https://stat.gov.kz/en/industries/economy/prices/dynamic-tables/"
 
-icon = tk.PhotoImage(file='./static/icon.png')
+icon = tk.PhotoImage(file='../static/icon.png')
 root.iconphoto(True, icon)
 
 root.mainloop()
